a03e50b7ffe7696b1e769fb5eeded3fbd121a6c7
Pip Turner <pipturner@outlook.com>
2018-02-22 15:05:27 +0000

    Deleted Terrain Controller, replaced with DiamondSquare class.

---
+++ Controllers/DiamondSquare.cpp
@@ -0,0 +1,57 @@
+//
+// Created by pip on 22/02/18.
+//
+
+#include "DiamondSquare.h"
+#include <stdlib.h>     /* srand, rand */
+#include <iostream>
+
+int width;
+int height;
+
+
+int randInRange(int range) {
+    srand(time(NULL));
+    return rand() % range + 1;
+}
+
+void diamond_step(int x, int y, int step_size, int r, float heightMap) {
+    // float avg = (x + )
+}
+
+void square_step(int x, int y, int step_size, int r, float heightMap) {
+
+}
+
+void divide(int size, float heightMap[]) {
+
+    int halfSize = size / 2;
+
+    randNum = randInRange(range);
+
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < y; j++) {
+
+        }
+    }
+
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < y; j++) {
+
+        }
+    }
+
+    stepSize /= 2;
+    range /= 2;
+}
+
+
+void generateHeightMap(int x, int y, int range) {
+    float heightMap[x][y] = {};
+    int stepSize = x - 1;
+    int randNum;
+
+    divide(x - 1, *heightMap);
+}
+
+
---
+++ Controllers/DiamondSquare.h
@@ -0,0 +1,14 @@
+//
+// Created by pip on 22/02/18.
+//
+
+#ifndef INC_3DPGNS_DIAMONDSQUARE_H
+#define INC_3DPGNS_DIAMONDSQUARE_H
+
+
+class DiamondSquare {
+
+};
+
+
+#endif //INC_3DPGNS_DIAMONDSQUARE_H
