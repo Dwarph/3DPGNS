f47d2dbb32cbb00bd0abe8ace5d538d7fccd437b
Pip Turner <pipturner@outlook.com>
2018-03-23 12:53:10 +0000

    Added empty Lindenmayer System Class

---
+++ L_System.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pip on 22/03/18.
+//
+
+#include "L_System.h"
---
+++ L_System.h
@@ -0,0 +1,14 @@
+//
+// Created by pip on 22/03/18.
+//
+
+#ifndef INC_3DPGNS_L_SYSTEM_H
+#define INC_3DPGNS_L_SYSTEM_H
+
+
+class L_System {
+
+};
+
+
+#endif //INC_3DPGNS_L_SYSTEM_H
